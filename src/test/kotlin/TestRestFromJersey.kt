2d0012d7ed4b316ddd92c20a9b0948fb658c9bc1
Arek Kaczorowski <mail>
2018-10-28 11:17:42 +0100

    Added CompletableFuture example.

--- src/test/kotlin/TestRestFromJersey.kt
+++ src/test/kotlin/TestRestFromJersey.kt
@@ -1,14 +1,17 @@
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.CompletableFuture
+import java.util.function.Supplier
 import javax.ws.rs.client.ClientBuilder
+import javax.ws.rs.client.Entity
 import javax.ws.rs.client.WebTarget
 
 
 class TestRestFromJersey {
 
 
-    private var target: WebTarget? = null
+    private lateinit var target: WebTarget
 
     data class Greeting(var id: Long = 0, var content: String = "")
 
@@ -28,14 +31,34 @@ class TestRestFromJersey {
 
     @Test
     fun test() {
-        val responseMsg = target!!.path("hello").request().get(String::class.java)
+        val responseMsg = target.path("hello").request().get(String::class.java)
         println(responseMsg)
     }
 
     @Test
     fun test2() {
-        val responseMsg = target!!.path("hello").request().get(Greeting::class.java)
+        val responseMsg = target.path("hello").request().get(Greeting::class.java)
         //  val responseMsg = target!!.path("hello").request(MediaType.APPLICATION_JSON_TYPE).get(Greeting::class.java)
         println(responseMsg)
+    }
+
+    @Test
+    fun test3() {
+        val response = target.path("message").request().post(Entity.text("Hello"))
+
+        println(response)
+    }
+
+    @Test
+    fun test4() {
+        val supplier: Supplier<Greeting> = Supplier { target.path("hello").request().get(Greeting::class.java)}
+
+        CompletableFuture.supplyAsync(supplier).thenAccept(this::consume).get()
+        //  val responseMsg = target!!.path("hello").request(MediaType.APPLICATION_JSON_TYPE).get(Greeting::class.java)
+//        println(responseMsg)
+    }
+
+    fun consume(greeting: Greeting){
+        target.path("message").request().post(Entity.json(greeting))
     }
 }
